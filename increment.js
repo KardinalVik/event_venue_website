51db827266a9699a9db3bda36d8d820b226d2bb2
kardinalvik <kardinalv@proton.me>
2024-10-20 23:41:10 -0400

    Updated footer

--- increment.js
+++
@@ -1,9 +0,0 @@
-const minusBtn = document.querySelector('.minus');
-const plusBtn = document.querySelector('.plus');
-
-minusBtn.addEventListener('click', (e) => {
-    let el = document.getElementById("TR").value;
-    el += 1;
-    document.getElementById("TR").textContent = el;
-    //e.preventDefault();
-});

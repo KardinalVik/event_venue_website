5379af12e8aa88c7a01c27012b6704476ba86bd0
KardinalVik <kardinalv@proton.me>
2024-11-07 20:09:43 -0500

    Added comments

--- script.js
+++ script.js
@@ -6,23 +6,23 @@
     and allow for return to the page */
 
 function initializeImageModal() {
-    const images = document.querySelectorAll("img:not(.not_clickable)");
-    const modal = document.getElementById("modal");
-    const modal_image = document.getElementById("modal_content");
+    const images = document.querySelectorAll("img:not(.not_clickable)"); // Find all clickable images on page
+    const modal = document.getElementById("modal"); // Find the dormant modal
+    const modal_image = document.getElementById("modal_content"); // Find the img tag in the modal
 
-    images.forEach(img => {
-        img.addEventListener("click", (e) => {
-            modal.style.display = "block";
-            modal_image.src = e.target.src;
-            document.body.style.overflow = "hidden"; // Prevent scrolling
+    images.forEach(img => { // Creates an event listener on every clickable image previously found
+        img.addEventListener("click", (e) => { // When an image is clicked...
+            modal.style.display = "block"; // Make modal visible
+            modal_image.src = e.target.src; // Set the img in the modal to the same image as was clicked on
+            document.body.style.overflow = "hidden"; // Make page not scrollable
         });
     });
 
-    modal.addEventListener("click", () => {
-        modal.style.display = "none";
+    modal.addEventListener("click", () => { // Creates an event listener on the modal
+        modal.style.display = "none"; // When clicked, make modal non-visible again
         document.body.style.overflow = "auto"; // Restore scrolling
     });
 }
 
-document.addEventListener("DOMContentLoaded", initializeImageModal);
+document.addEventListener("DOMContentLoaded", initializeImageModal); // Calls above function when page is loaded
 // ----------
